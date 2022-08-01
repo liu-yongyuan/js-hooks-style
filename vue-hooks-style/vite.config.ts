2666a81aa2f3e3c12c9c5f1452f33525c09f382d
yongyuan <yongyuanliu2022@outlook.com>
2022-08-01 17:36:54 +0800

    添加 element-plus 并 按需引入

--- vue-hooks-style/vite.config.ts
+++ vue-hooks-style/vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import DefineOptions from 'unplugin-vue-define-options/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => {
-  return { plugins: [vue(), DefineOptions()] }
+  return {
+    plugins: [
+      vue(),
+      DefineOptions(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  }
 })
